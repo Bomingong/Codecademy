004c8d9619922996439760e69faaac1032d96128
Christopher Ong <Bomingong@gmail.com>
2018-08-25 02:56:51 -0400

    commit ravenous part 2

--- ravenous/src/components/App/App.js
+++ ravenous/src/components/App/App.js
@@ -3,13 +3,30 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList.js';
 import SearchBar from '../SearchBar/SearchBar.js';
 
+// Hardcoded business listing
+// Yelp API will return a list of businesses to App.js
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+// hardcoded array of "returned" businesses
+const businesses = [business, business, business, business, business, business];
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses} />
       </div>
     );
   }
